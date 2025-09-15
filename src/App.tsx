9d57529a89c2846a47c434df1757dfa0c57342d3
Nathan <nathanasowata@gmail.com>
2025-09-15 13:35:46 +0100

    feat: removed the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Uploader from './components/Uploader';
 import DownloadPage from './components/DownloadPage';
-import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <Header />
       <Routes>
         <Route path="/" element={<Uploader />} />
         <Route path="/view/:shortId" element={<DownloadPage />} />
